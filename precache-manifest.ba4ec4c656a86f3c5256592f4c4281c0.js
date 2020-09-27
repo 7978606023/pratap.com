429fede93c20ab68b377f7de72e2b7ac20ecf542
Pratap <51462413+7978606023@users.noreply.github.com>
2020-09-27 13:56:16 -0700

    Add files via upload

---
+++ precache-manifest.ba4ec4c656a86f3c5256592f4c4281c0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "519b271e8575353f76b102c0ed53811c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b256b28f9dd2039d4b8d",
+    "url": "./static/css/2.2058ee32.chunk.css"
+  },
+  {
+    "revision": "7f5ef9e1247f1854ddfa",
+    "url": "./static/css/main.db5ed23f.chunk.css"
+  },
+  {
+    "revision": "b256b28f9dd2039d4b8d",
+    "url": "./static/js/2.0b1f15a1.chunk.js"
+  },
+  {
+    "revision": "7f5ef9e1247f1854ddfa",
+    "url": "./static/js/main.ea36def5.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "d902400cd7f58620150ce21a91ea9b8c",
+    "url": "./static/media/Vmarine.d902400c.jpg"
+  },
+  {
+    "revision": "7656ed07a28aa28d65caf29ddc4dc1b9",
+    "url": "./static/media/aguaDeLuz.7656ed07.png"
+  },
+  {
+    "revision": "ac1aceade9946caaf83867ea7b70f2a0",
+    "url": "./static/media/myImage.ac1acead.png"
+  },
+  {
+    "revision": "3f235a834794b88f67a959880b5bdbb7",
+    "url": "./static/media/todolist.3f235a83.png"
+  }
+]);
